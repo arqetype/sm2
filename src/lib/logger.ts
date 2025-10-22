571ea339a3f980700fcc36d8097751ebf1c78fbb
Pierre Gueroult <pierregueroult2022@gmail.com>
2025-10-22 14:34:27 +0200

    feat: add colors to console logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -7,6 +7,18 @@ interface LogEntry {
   data?: unknown;
 }
 
+// ANSI color codes
+const colors = {
+  reset: '\x1b[0m',
+  gray: '\x1b[90m',
+  blue: '\x1b[34m',
+  cyan: '\x1b[36m',
+  yellow: '\x1b[33m',
+  red: '\x1b[31m',
+  magenta: '\x1b[35m',
+  bold: '\x1b[1m'
+};
+
 class Logger {
   private context: string;
 
@@ -22,8 +34,29 @@ class Logger {
       data
     };
 
-    const prefix = `[${entry.timestamp}] [${level.toUpperCase()}] [${this.context}]`;
-    const output = data ? `${prefix} ${message}` : `${prefix} ${message}`;
+    let levelColor = colors.reset;
+    const contextColor = colors.cyan;
+
+    switch (level) {
+      case 'debug':
+        levelColor = colors.gray;
+        break;
+      case 'info':
+        levelColor = colors.blue;
+        break;
+      case 'warn':
+        levelColor = colors.yellow;
+        break;
+      case 'error':
+        levelColor = colors.red;
+        break;
+    }
+
+    const timestamp = `${colors.gray}[${entry.timestamp}]${colors.reset}`;
+    const levelStr = `${levelColor}[${level.toUpperCase()}]${colors.reset}`;
+    const context = `${contextColor}[${this.context}]${colors.reset}`;
+    const prefix = `${timestamp} ${levelStr} ${context}`;
+    const output = `${prefix} ${message}`;
 
     switch (level) {
       case 'debug':
