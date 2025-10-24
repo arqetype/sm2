9a73625b1a1a3515d185ff5223d76f7634319906
Pierre Gueroult <pierregueroult2022@gmail.com>
2025-10-24 17:56:52 +0200

    feat: add tabs to the sidebar

--- src/renderer/src/components/sidebar/app-sidebar.tsx
+++ src/renderer/src/components/sidebar/app-sidebar.tsx
@@ -1,4 +1,5 @@
 import { Sidebar, SidebarContent, SidebarFooter } from '../ui/sidebar';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tabs';
 
 export function AppSidebar() {
   return (
@@ -6,7 +7,16 @@ export function AppSidebar() {
       variant="sidebar"
       className="h-[calc(100svh-calc(var(--spacing)*9))] top-9 bg-vibrancy border-t"
     >
-      <SidebarContent>This is sidebar content</SidebarContent>
+      <SidebarContent>
+        <Tabs defaultValue="decks">
+          <TabsList className="grid grid-cols-2 w-full bg-transparent p-2">
+            <TabsTrigger value="decks">Decks</TabsTrigger>
+            <TabsTrigger value="files">Courses</TabsTrigger>
+          </TabsList>
+          <TabsContent value="decks">Those are your decks</TabsContent>
+          <TabsContent value="files">Those are your files</TabsContent>
+        </Tabs>
+      </SidebarContent>
       <SidebarFooter>This is a sidebar footer</SidebarFooter>
     </Sidebar>
   );
